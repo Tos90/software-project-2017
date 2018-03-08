e6b7480295ad5497a3fe54c782fdc5de66440f22
Tomas O Sullivan <115746009@umail.ucc.ie>
2018-03-08 22:51:08 +0000

    j

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from flask import Flask,render_template, redirect, url_for, session, request, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_table import Table, Col, LinkCol
-from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 
