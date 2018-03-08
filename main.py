0138b1594f820d5b7454645923abf86e6d8b164b
Tomas O Sullivan <115746009@umail.ucc.ie>
2018-03-08 23:35:56 +0000

    j

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'secret'
 
 @app.route('/hello')
 def hello_world():
