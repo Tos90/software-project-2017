040d5e123c0a04e112155fb9e46027a7aef4f826
Tomas O Sullivan <115746009@umail.ucc.ie>
2018-03-08 23:43:27 +0000

    k

--- main.py
+++ main.py
@@ -112,15 +112,6 @@ class ActiveUsers(Base, db.Model):
     def __init__(self):
         self.username = current_user.username
 
-class LoginForm(FlaskForm): #define login form for bootstrap
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
-class RegisterForm(FlaskForm): #define registration form for bootstrap
-    email = StringField('email', validators=[InputRequired(), Length(max=50), Email("This field requires a valid email address")])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
 @app.route('/hello')
 def hello_world():
   return render_template('index.html')
