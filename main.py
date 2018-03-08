229c9c0e99490654cd2e97acc52fb46ab82dffc5
Tomas O Sullivan <115746009@umail.ucc.ie>
2018-03-08 16:53:06 +0000

    a

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from flask import Flask,render_template, redirect, url_for, session, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_table import Table, Col, LinkCol
-from sqlalchemy.ext.declarative import declarative_base
-from time import gmtime, strftime
-from dealer import Dealer
-from deck import Deck
-import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
